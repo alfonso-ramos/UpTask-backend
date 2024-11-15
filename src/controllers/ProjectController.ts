b7cb3acc429b5ff653019d71ae5dfac6c0061a42
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-11-14 23:16:04 -0700

    commit solo para tener respaldo

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,17 @@
+import type { Request, Response} from "express"
+import Project from "../models/Project"
+export class ProjectController {
+
+    static createProject = async (req: Request, res: Response) => {
+        const project = new Project(req.body)
+        try {
+            await project.save()
+            res.send("Project has been created")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static getAllProjects = async (req: Request, res: Response) => {
+        res.send('All the projects')
+    }
+}
